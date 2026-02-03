311b97d350c9a1173ae9f947fcbcd20fa655b0ef
reginabeltran <reginabeltran06@gmail.com>
2026-02-02 21:25:20 -0600

    homework 02.3

---
+++ Homeworks/Homework2/src/Main.java
@@ -0,0 +1,29 @@
+package Homework02;
+
+import javax.swing.*;
+import java.util.List;
+import Homework02.ui.Panel;
+import Homework02.converters.*;
+
+public class Main extends JFrame {
+
+    public Main() {
+
+        setTitle("Polar to Cartesian Converter");
+        setSize(820, 420);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        List<Converter> converters = List.of(
+                new PolarToCartesian(),
+                new CartesianToPolar()
+        );
+
+        add(new Panel(converters));
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Main::new);
+    }
+}
